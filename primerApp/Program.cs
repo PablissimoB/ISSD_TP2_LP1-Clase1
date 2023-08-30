6bf7c8fe3d399b3db951c5f05d8d75f304fe4040
Pablo Bobarini <50304993+PablissimoB@users.noreply.github.com>
2023-08-30 17:31:09 -0300

    Aplicacion consola para aplicar conceptos basicos en C#

--- primerApp/Program.cs
+++ primerApp/Program.cs
@@ -6,6 +6,27 @@ using System.Threading.Tasks;
 
 namespace primerApp
 {
+    // declaracion de clases Ejercitacion
+     class Institucion
+    {
+        //declaracion de atributos
+        public string Name;
+        public DateTime fechaFundacion;
+        
+        //metodo constructor
+        public Institucion (string name, DateTime fechaFundacion)
+        {
+            this.Name = name;
+            this.fechaFundacion = fechaFundacion;
+        }
+        //funcion
+        public int cantidadAnios()
+        {
+            return DateTime.Now.Year - fechaFundacion.Year;
+        }
+
+    }
+    // declaracion de clases
     class Persona
     {
         public string nombre;
@@ -24,41 +45,71 @@ namespace primerApp
         }
     }
 
+    //Clase Program
     class Program
     {
+        //funcion que recibe un argumento
         string Saludo(string arg)
         {
             return "Hola desde funcion de "+arg;
         }
+        //Funcion estatica
         static string SaludoEstatico()
         {
             return "Hola desde funcion estatico";
         }
+        //Metodo
         void Saludar()
         {
             Console.WriteLine("Hola desde metodo ejecutado por instancia de clase Program");
         }
 
+        //Funcion ejercitacion
+        static int potencia (int basePotencia, int exponente)
+        {
+            int resultado = 1;
+            //uso de for
+            for(int i = 0; i < exponente; i++)
+            {
+                resultado = resultado* basePotencia;
+            }
+
+            return resultado;
+        }
+  
+
         static void Main(string[] args)
         {
-            Program programa = new Program();
+            Program programa = new Program(); //creacion de objeto de clase Program
+            DateTime fecha = new DateTime(2000,12,2);
+            Institucion institucion = new Institucion("ISSD", fecha);
+            Console.WriteLine(institucion.cantidadAnios());
             Console.WriteLine("Hola Mundo");//Saludo Harcodeado
             Console.WriteLine(programa.Saludo("objetoProgram"));//Saludo desde impresion de metodo de objetoProgram
             Console.WriteLine(SaludoEstatico());//Saludo desde impresion de funcion estatico
             programa.Saludar();//Saludo desde metodo de objeto programa
-            Persona persona = new Persona("Pedro",25);
+            Persona persona = new Persona("Pedro", 25);
             persona.Saludar();//saludo desde metodo de objeto persona
-            if(persona.nombre == "Pablo")
+            // uso de if
+            if (persona.nombre == "Juan")
             {
-                Console.WriteLine("Hola Pablo");
+                Console.WriteLine("Hola Juan");
+            }
+            else if (persona.nombre == "Pedro")
+            {
+
             }
             else
             {
-                Console.WriteLine("Hola "+persona.nombre);//saludo desde condicional
+                Console.WriteLine("Hola " + persona.nombre);//saludo desde condicional
             }
+            //uso de operador ternario
+            string mayorDe18 = persona.edad > 18 ? "es mayor" : "es menor";
+            Console.WriteLine(mayorDe18);
+            //uso de switch
             switch (persona.nombre)
             {
-                case "Pablo": Console.WriteLine("Hola Pablo");break;
+                case "Pablo": Console.WriteLine("Hola Pablo"); break;
                 case "Pedro": Console.WriteLine("Hola Pedro"); break;
                 case "default": Console.WriteLine("Hola Persona no identificada"); break;
             }//saludo desde switch
@@ -74,9 +125,11 @@ namespace primerApp
             bool verificador = true;
             int edad = persona.edad;
             int contadorLustros = 0;
+            //uso de while
             while (verificador)
             {
                 contadorLustros++;
+                Console.WriteLine(edad);
                 edad -= 5;
                 if (edad == 0)
                 {
@@ -84,9 +137,10 @@ namespace primerApp
                     Console.WriteLine(contadorLustros);
                 }
             }
+            //uso de foreach con argumentos del metodo Main
             foreach (string arg in args)
             {
-                Console.WriteLine("Argumento: "+arg);
+                Console.WriteLine("Argumento: " + arg);
             }
         }
     }
